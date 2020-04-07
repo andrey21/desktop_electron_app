3bc2ecf461c1a324655b48df8a375f5ab8e7ae87
AndreyZ <zareckijandrej1@gmail.com>
2020-04-07 17:41:09 +0300

    added server-side script used to serve the request from all connected clients

--- server/server.js
+++ server/server.js
@@ -1,6 +1,51 @@
 let express = require('express');
 let app = express();
+// var http = require('http').Server(app);
+var io = require('socket.io')(server);
+var path = require('path');
 
-app.listen(3000, () => {
+var onlineUsers = [];
+
+// Initialize appication with route / (that means root of the application)
+app.get('/', (req, res) => {
+  app.use(express.static(path.join(__dirname)));
+  res.sendFile(path.join(__dirname, '../chat-application', 'index.html'));
+});
+
+// Register events on socket connection
+io.on('connection', (socket) => { 
+
+  // Listen to chantMessage event sent by client and emit a chatMessage to the client
+  socket.on('chatMessage', (message) => {
+    io.to(message.receiver).emit('chatMessage', message);
+  });
+
+  // Listen to notifyTyping event sent by client and emit a notifyTyping to the client
+  socket.on('notifyTyping', (sender, receiver) => {
+    io.to(receiver.id).emit('notifyTyping', sender, receiver);
+  });
+
+  // Listen to newUser event sent by client and emit a newUser to the client with new list of online users
+  socket.on('newUser', (user) => {
+    var newUser = {id: socket.id, name: user};
+    onlineUsers.push(newUser);
+    io.to(socket.id).emit('newUser', newUser);
+    io.emit('onlineUsers', onlineUsers);
+  });
+
+  // Listen to disconnect event sent by client and emit userIsDisconnected and onlineUsers (with new list of online users) to the client 
+  socket.on('disconnect', () => {
+    onlineUsers.forEach((user, index) => {
+      if(user.id === socket.id) {
+        onlineUsers.splice(index, 1);
+        io.emit('userIsDisconnected', socket.id);
+        io.emit('onlineUsers', onlineUsers);
+      }
+    });
+  });
+
+});
+
+var server = app.listen(3000, () => {
     console.log('server is running on port 3000');
 })
